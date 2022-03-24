ce4a9e6108a0681b3c965f440d36d62b728d22ac
LunaticCat <yannbrun1507@outlook.fr>
2022-03-24 15:05:56 +0100

    Continued network project

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,17 +1,19 @@
-import javax.crypto.*;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        boolean stopped = false, hasFoundServer = false;
-        String messageOut;
-        String messageIn;
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        boolean hasFoundServer = false;
         Socket socket = null;
         System.out.println("Recherche d'un serveur...");
 
@@ -29,35 +31,42 @@ public class Client {
             } catch (IOException ignored) {
             }
         }
-        System.out.println("Server found !");
 
-        PrintWriter outs = new PrintWriter(
-                new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-        BufferedReader ins = new BufferedReader(
-                new InputStreamReader(socket.getInputStream()));
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
         Scanner scanner = new Scanner(System.in);
 
-        Cipher ci = Cipher.getInstance("RSA");
-        ci.init(Cipher.DECRYPT_MODE, privateKey);
+        System.out.println("Server found !");
 
-        outs.println(new String(publicKey.getEncoded()));
+        out.write(publicKey.getEncoded());
 
-        Key secretKey = new SecretKeySpec(ci.doFinal(ins.readLine().getBytes()), "DES");
-        Cipher cipher = Cipher.getInstance("DES");
+        byte[] CleDesIn = new byte[128];
 
-        while (!stopped) {
+        // Récupère la clé secrète encodé
+        in.read(CleDesIn, 0, 128);
+        // Décode la clé secrète en utilisant la clé privée
+        Cipher cipherRSA = Cipher.getInstance("RSA");
+        cipherRSA.init(Cipher.DECRYPT_MODE, privateKey);
+        // décode la clé DES, encore sous la forme de tableau de bytes
+        byte[] CleDesByte = cipherRSA.doFinal(CleDesIn);
+        // transforme le tableau de bytes décode en clé secrète DES
+        Key CleDES = new SecretKeySpec(CleDesByte, 0, CleDesByte.length, "DES");
+
+        Cipher cipherDES = Cipher.getInstance("DES");
+
+        while (true) {
+            String messageOut, messageIn;
+            byte[] messageInBytes = new byte[128];
+
             messageOut = scanner.nextLine();
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            messageOut = new String(cipher.doFinal(messageOut.getBytes()));
-            outs.println(messageOut);
-            messageIn = ins.readLine();
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            messageIn = new String(cipher.doFinal(messageIn.getBytes()));
+            cipherDES.init(Cipher.ENCRYPT_MODE, CleDES);
+            out.write(cipherDES.doFinal(messageOut.getBytes()));
+            System.out.println(messageOut.length());
+
+            in.read(messageInBytes, 0, 128);
+            cipherDES.init(Cipher.DECRYPT_MODE, CleDES);
+            messageIn = new String(cipherDES.doFinal(messageInBytes));
             System.out.println(messageIn);
-            if (messageIn.equals("stop") || messageOut.equals("stop")) {
-                stopped = true;
-            }
         }
-        System.out.println("Client termination");
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,64 +1,52 @@
 import javax.crypto.*;
-import javax.crypto.spec.SecretKeySpec;
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Server {
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        boolean stopped = false;
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         ServerSocket serverSocket = new ServerSocket(22222);
         Socket socket = serverSocket.accept();
-        BufferedReader ins = new BufferedReader(
-                new InputStreamReader(socket.getInputStream()));
-        PrintWriter outs = new PrintWriter(
-                new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        Scanner scanner = new Scanner(System.in);
 
         KeyGenerator keyGenerator = KeyGenerator.getInstance("DES");
         keyGenerator.init(56);
         Key secretKey = keyGenerator.generateKey();
 
-        char[] characters = new char[1024];
-        int pos = 0;
-        do {
-            pos += ins.read(characters);
-        } while (pos < 1024);
-        String receivedKey = new String(characters);
-        System.out.println(receivedKey);
-        Key clientKey = new SecretKeySpec(receivedKey.getBytes(StandardCharsets.UTF_8), "RSA");
-        Cipher ci = Cipher.getInstance("RSA");
-        ci.init(Cipher.ENCRYPT_MODE , clientKey);
-        byte[] keyToSend = secretKey.toString().getBytes();
-        outs.println(new String(ci.doFinal(keyToSend)));
+        byte[] ClePubRsa = new byte[1024];
+        in.read(ClePubRsa, 0, 1024);
+        PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(ClePubRsa));
 
-        Scanner scanner = new Scanner(System.in);
+        Cipher cipherRSA = Cipher.getInstance("RSA");
+        cipherRSA.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] CleDesOut = cipherRSA.doFinal(secretKey.getEncoded());
 
-        String messageIn;
-        String messageOut;
-        Cipher cipher = Cipher.getInstance("DES");
+        out.write(CleDesOut);
 
-        while (!stopped) {
-            messageIn = ins.readLine();
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            messageIn = new String(cipher.doFinal(messageIn.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
+        Cipher cipherDES = Cipher.getInstance("DES");
+
+        while(true) {
+            String messageOut, messageIn;
+            byte[] messageInBytes = new byte[128];
+
+            in.read(messageInBytes, 0, 128);
+            cipherDES.init(Cipher.DECRYPT_MODE, secretKey);
+            messageIn = new String(cipherDES.doFinal(messageInBytes));
+            System.out.println(messageIn);
+
             messageOut = scanner.nextLine();
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            messageOut = new String(cipher.doFinal(messageOut.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
-            outs.println(messageOut);
-            outs.println(messageOut);
-            if (messageIn.equals("stop") || messageOut.equals("stop")) {
-                stopped = true;
-            }
-        }
+            cipherDES.init(Cipher.ENCRYPT_MODE, secretKey);
+            out.write(cipherDES.doFinal(messageOut.getBytes()));
 
-        ins.close();
-        outs.close();
-        socket.close();
+        }
     }
 }
